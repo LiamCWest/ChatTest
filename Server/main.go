781a40d6c85f0748a6eaf4493c0834a7b3ad7820
archiso <awesomelax7@gmail.com>
2023-09-07 16:30:54 -0400

    more stuff

---
+++ Server/main.go
@@ -0,0 +1,40 @@
+package Server
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/LiamCWest/ChatTest/api/v1"
+	"google.golang.org/grpc"
+)
+
+type gameServer struct {
+	pb.UnimplementedGameServiceServer
+}
+
+func (s *gameServer) AddPlayer(ctx context.Context, req *pb.Player) (*pb.Player, error) {
+	return &pb.Player{}, nil
+}
+
+func (s *gameServer) GetPlayer(ctx context.Context, req *pb.PlayerID) (*pb.Player, error) {
+	return &pb.Player{}, nil
+}
+
+func (s *gameServer) MovePlayer(ctx context.Context, req *pb.PlayerMovement) (*pb.Player, error) {
+	return &pb.Player{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGameServiceServer(s, &gameServer{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module Server
 
 go 1.21.0
 
-require google.golang.org/protobuf v1.31.0
+require (
+    google.golang.org/protobuf v1.31.0
+    google.golang.org/grpc v1.31.0
+)
